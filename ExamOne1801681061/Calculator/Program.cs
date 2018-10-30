b83df7609fb3cec4fce90bf5cda620cc68988dec
konstantinpetkow <tinkoci@abv.bg>
2018-10-30 17:57:57 +0200

    Finished Exam1

---
+++ ExamOne1801681061/Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 12.78;
+
+            string operatorche = Console.ReadLine();
+
+            switch (operatorche)
+            {
+                case "+":
+                    double sum = a + b;
+                    double withPercentage = sum + (sum * (10 / 100));
+                    Console.WriteLine($"Result is: {withPercentage}");
+                    break;
+
+                case "-":
+                    break;
+                case "*":
+                    break;
+                case "/":
+                    break;
+
+
+
+
+                default:
+                    Console.WriteLine("Error 404. No such operator");
+                    break;
+            }
+
+        }
+    }
+}
