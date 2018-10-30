3d06ae1cd4e285152b5f985eb4d665246db39de6
konstantinpetkow <tinkoci@abv.bg>
2018-10-30 17:38:49 +0200

    Finished Exam1

---
+++ ExamOne1801681061/PiggyBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PiggyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            double sum = 0;
+
+            Console.WriteLine("Enter a real number:");
+            string input = Console.ReadLine();
+
+            int inputNum;
+            while (!int.TryParse(input, out inputNum))
+            {
+                Console.WriteLine("You have to enter a valid real number!");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
