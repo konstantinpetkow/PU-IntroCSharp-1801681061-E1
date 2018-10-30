a30a1332d4a2401871b7d731a164ade91ab8ed16
konstantinpetkow <tinkoci@abv.bg>
2018-10-30 16:44:45 +0200

    inlcudet Exam1

---
+++ ExamOne1801681061/RectangleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int Count1 = rnd.Next(33, 187);
+            int Count2 = rnd.Next(33, 187);
+
+            int area = Count1 * Count2;
+            Console.WriteLine($"Rectangle area is {area:0}");
+        }
+    }
+}
